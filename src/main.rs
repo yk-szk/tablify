2e144247e0c75b2fec56a8a7daed6ff9e66d9f2e
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2021-03-07 16:38:04 +0900

    Add vec

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,21 @@ use std::fs::File;
 use tera::Tera;
 
 
-fn example(file_path: &str) -> Result<(), Box<dyn Error>> {
+fn example(file_path: &str) -> Result<(Vec<csv::StringRecord>), Box<dyn Error>> {
     let file = File::open(file_path)?;
     let mut rdr = csv::Reader::from_reader(file);
+    let mut v: Vec<csv::StringRecord> = Vec::new();
     for result in rdr.records() {
-        let record = result?;
-        println!("{:?}", record);
+        if let Ok(r) = result {
+            println!("{:?}", r);
+            v.push(r);
+        }
     }
-    Ok(())
+    Ok(v)
 }
 
 fn main() {
-    let matches = App::new("My Super Program")
+    let matches = App::new("tablify")
         .version(crate_version!())
         .author("Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>")
         .about("Load tabular data and render a html file.")
@@ -27,12 +30,6 @@ fn main() {
                 .required(true)
                 .index(1),
         )
-        .arg(
-            Arg::with_name("v")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of verbosity"),
-        )
         .get_matches();
 
     // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
@@ -40,37 +37,18 @@ fn main() {
     let input = matches.value_of("INPUT").unwrap();
     println!("Using input file: {}", input);
 
-    let tera = match Tera::new("../table.html") {
-        Ok(t) => t,
-        Err(e) => {
-            println!("Parsing error(s): {}", e);
-            ::std::process::exit(1);
-        }
-    };
+    // let tera = match Tera::new("../table.html") {
+    //     Ok(t) => t,
+    //     Err(e) => {
+    //         println!("Parsing error(s): {}", e);
+    //         ::std::process::exit(1);
+    //     }
+    // };
 
-    // Vary the output based on how many times the user used the "verbose" flag
-    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-    match matches.occurrences_of("v") {
-        0 => println!("No verbose info"),
-        1 => println!("Some verbose info"),
-        2 => println!("Tons of verbose info"),
-        3 | _ => println!("Don't be crazy"),
-    }
-
-    // You can handle information about subcommands by requesting their matches by name
-    // (as below), requesting just the name used, or both at the same time
-    if let Some(matches) = matches.subcommand_matches("test") {
-        if matches.is_present("debug") {
-            println!("Printing debug info...");
-        } else {
-            println!("Printing normally...");
-        }
-    }
     if let Err(err) = example(input) {
         println!("error running example: {}", err);
         std::process::exit(1);
     }
 
     std::process::exit(0)
-    // more program logic goes here...
 }
