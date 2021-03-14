f81624248e3c34d22562d0b084df72b5e3a1bc00
Yuki Suzuki <y-suzuki@radiol.med.osaka-u.ac.jp>
2021-03-14 16:18:00 +0900

    Fix missing header issue

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,9 @@ pub fn tablify(
 pub fn load_csv(a: &[u8]) -> Result<Vec<Vec<String>>, csv::Error> {
     let (csv_content, _, _) = encoding_rs::SHIFT_JIS.decode(&a);
     let csv_content = csv_content.into_owned();
-    let mut rdr = csv::Reader::from_reader(csv_content.as_bytes());
+    let mut rdr = csv::ReaderBuilder::new()
+        .has_headers(false)
+        .from_reader(csv_content.as_bytes());
     let mut rows: Vec<Vec<String>> = Vec::new();
     for result in rdr.deserialize() {
         let row: Vec<String> = result?;
@@ -66,7 +68,9 @@ pub fn load_xlsx(a: &[u8]) -> Result<Vec<Vec<String>>, calamine::Error> {
     let range = workbook
         .worksheet_range(&sheet_name)
         .ok_or(calamine::Error::Msg("Cannot find a sheet."))??;
-    let mut iter = RangeDeserializerBuilder::new().from_range(&range)?;
+    let mut iter = RangeDeserializerBuilder::new()
+        .has_headers(false)
+        .from_range(&range)?;
     while let Some(result) = iter.next() {
         let row: Vec<String> = result?;
         rows.push(row);
@@ -98,6 +102,6 @@ mod tests {
             .collect::<Vec<_>>()
             .join("\n");
         let loaded_rows = load_csv(csv_content.as_bytes()).unwrap();
-        assert_eq!(rows.len(), loaded_rows.len());
+        assert_eq!(rows, loaded_rows);
     }
 }
